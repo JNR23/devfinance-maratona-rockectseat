55f837ac4958f4ed0e963056c41e92baa056931f
JNR23 <junior_bhzte@hotmail.com>
2021-04-20 16:19:23 -0300

    Inserção de dados no Modal e remoção de itens

--- scripts.js
+++ scripts.js
@@ -71,12 +71,13 @@ const DOM = {
 
     addTransaction(transaction, index) {
         const tr = document.createElement('tr');
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+        tr.dataset.index = index;
 
         DOM.transactionsContainer.appendChild(tr);
     },
 
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
         const amount = Utils.formatCurrency(transaction.amount);
@@ -86,7 +87,7 @@ const DOM = {
         <td class="${CSSclass}">${amount}</td>
         <td class="date">${transaction.date}</td>
         <td>
-            <img src="./assets/minus.svg" alt="Remover transação">
+            <img src="./assets/minus.svg" alt="Remover transação" onclick="Transaction.remove(${index})">
         </td>
         `
         return html;
@@ -104,6 +105,16 @@ const DOM = {
 }
 
 const Utils = {
+    formatAmount(value) {
+        value = Number(value) * 100;
+        return value;
+    },
+
+    formatDate(date) {
+        const splittedDate = date.split("-");
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+    },
+
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : "";
 
@@ -119,10 +130,66 @@ const Utils = {
     }
 }
 
+const Form = {
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+
+    validateFields() {
+        const { description, amount, date } = Form.getValues();
+
+        if (description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+            throw new Error("Preencha todos os campos");
+        }
+    },
+
+    formatValues() {
+        let { description, amount, date } = Form.getValues();
+
+        amount = Utils.formatAmount(amount);
+
+        date = Utils.formatDate(date);
+
+        return { description, amount, date }
+    },
+
+    clearFields() {
+        Form.description.value = "";
+        Form.amount.value = "";
+        Form.date.value = "";
+    },
+
+    submit(event) {
+        event.preventDefault();
+
+        try {
+            Form.validateFields();
+            const transaction = Form.formatValues();
+            Transaction.add(transaction);
+            Form.clearFields();
+            Modal.close();
+        } catch (error) {
+            alert(error.message);
+        }
+
+        Form.validateFields();
+
+        //Form.formatData();
+    }
+}
+
 const App = {
     init() {
-        Transaction.all.forEach((transaction) => {
-            DOM.addTransaction(transaction);
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index);
         });
 
         DOM.updateBalance();
@@ -135,11 +202,3 @@ const App = {
 }
 
 App.init();
-
-Transaction.add({
-    description: 'Alo',
-    amount: 200,
-    date: '23/01/2021'
-})
-
-Transaction.remove();
